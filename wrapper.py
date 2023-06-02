8dfebe11a79a8d8dde28f67856ef801f20ec5218
Daniel <danimalau@live.com>
2023-06-02 15:10:48 +0300

    cheching sparse matricies

--- wrapper.py
+++ wrapper.py
@@ -11,46 +11,61 @@ import json
 class Wrapper:
     def __init__(self):
         self.times = 25
-        self.d = 100
+        self.d = 20
         self.num_of_gausians = 2
-        self.num_of_samples = 10000
+        self.num_of_samples = 50000
         self.stats = Stats()
-        self.sparse = 5
+        self.sparse = 8
+        self.alinged = self.sparse
 
     def run(self):
-        means = self.gen_means(self.num_of_gausians,self.d)
-        covs = self.gen_covs(self.num_of_gausians,self.d)
-        X_full,Y_full = self.gen_data(self.num_of_samples,means,covs)
+        self.means = self.gen_means(self.num_of_gausians,self.d)
+        self.covs = self.gen_covs(self.num_of_gausians,self.d)
+        X_full,Y_full = self.gen_data(self.num_of_samples,self.means,self.covs)
 
         d = self.d
         X = X_full[:,:d]
         Y = Y_full
-        self.dump(X,Y,means,covs)
+
+        clf = mixture.GaussianMixture(n_components=self.num_of_gausians, covariance_type="full")
+        clf.fit(X)
+        # self.dump(X,Y,self.means,self.covs)
         for _run in range(self.times):
             phi = np.random.normal(0,1,(self.d,self.d))
-            # U, S ,VH = np.linalg.svd(phi_o)
-            # phi = U @ U.T
-
-           
-                # origin space
-            clf = mixture.GaussianMixture(n_components=self.num_of_gausians, covariance_type="full")
-            Z = clf.fit_predict(X)
+                       
+            # origin space
+            
+            clf.means_ = np.array(self.means)
+            clf.covariances_ = np.array(self.covs)
+            clf.weights_ = np.array([0.5,0.5])
+            Z = clf.predict(X)
             Z = self.permute(Y,Z,self.num_of_gausians)
             base_err = (Y != Z).sum()
-            for t in range(2,np.int32(np.floor(d/2))):    
+        
+            for t in range(2,d):    
                 self.run_one_time(t,d,phi[:d,:t],X,Y,base_err)
-            self.stats.store_resualts(f"res_sparse_{_run}.csv")
+        self.stats.store_resualts(f"res_sparse_alinged_cords.csv")
                     
                     
-    def run_one_time(self, t,d, phi,X,Y,base_err):
+    def run_one_time(self, t,d, phi_o,X,Y,base_err):
+        U, S ,VH = np.linalg.svd(phi_o,full_matrices=False)
+        phi = U 
+
+        means = self.means @ phi
+        covs = phi.T @ self.covs @ phi
+
         #  target space
         Xt = X @ phi
         clft = mixture.GaussianMixture(n_components=self.num_of_gausians, covariance_type="full")
-        Zt = clft.fit_predict(Xt)
+        clft.fit(Xt)
+        clft.means_ = np.array(means)
+        clft.covariances_ = np.array(covs)
+        clft.weights_ = np.array([0.5,0.5])
+        Zt = clft.predict(Xt)
         Zt = self.permute(Y,Zt,self.num_of_gausians)
         new_err = (Y != Zt).sum()
         self.stats.add_resualt(t,d,base_err / self.num_of_samples ,new_err / self.num_of_samples)
-        print(t,d,base_err / self.num_of_samples ,new_err / self.num_of_samples)
+        print(t,d,base_err ,new_err)
 
         
 
@@ -67,26 +82,39 @@ class Wrapper:
         return np.array(dataset),np.array(labels)
 
     def gen_means(self,size,d):
-        dataset = []
+        means = []
         for i in range(size):
             mean = np.random.normal(0,1,d)
-            dataset.append(mean)
-        return dataset
+            means.append(mean)
+        v1 = (1/np.sqrt(d)) * np.ones(d)
+        slice = np.arange(self.sparse,d)
+        v1[slice] = 0
+        v2 = -v1
+        # v2[:self.alinged] = -v2[:self.alinged]
+        return [v1,v2]
     
     def gen_covs(self,size,d):
-        dataset = []
+        covs = []
         for i in range(size):
-            sig = 5
+            sig = 4
             cov = sig * np.identity(d)
 
             if self.sparse != -1:
-                for i in range(d - self.sparse):
-                    ix = np.random.randint(d)
-                    while cov[ix,ix] == 0:
-                        ix = np.random.randint(d)
+                # for i in range(d - self.sparse):
+                #     ix = np.random.randint(d)
+                #     while cov[ix,ix] == 0:
+                #         ix = np.random.randint(d)
+                #     print(f"{ix},", end="")
+                #     cov[ix,ix] = 0
+                # print("")
+                # per = np.random.permutation(np.arange(d))
+                # slice = per[: (d - self.sparse)]
+                slice = np.arange(self.sparse,d)
+                for ix in slice:
                     cov[ix,ix] = 0
-            dataset.append(cov)
-        return dataset
+                    pass
+            covs.append(cov)
+        return covs
 
     def permute(self,Y,Z,k):
         from_values =  np.arange(0,max(Z)+1)
